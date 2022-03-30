3abd9928f47e745c391f5c9bb5464fba53989b5f
Bafarouk <44127908+Bafarouk@users.noreply.github.com>
2022-03-30 20:15:29 +0200

    Setting the project & test brownie

---
+++ scripts/deploy.py
@@ -0,0 +1,25 @@
+from brownie import SimpleStorage, accounts, config
+import os
+
+
+def deploy_simple_storage():
+    # getting the first address from 10 accounts that fanache generate :
+    account = accounts[0]
+
+    # getting account from the aded accounts with CLI ( DON'T DORGET THE PASSWORD ) :
+    # account = accounts.load("farouk-account")
+
+    # getting with the .env meth2 with wallets :
+    # account = accounts.add(config["wallets"]["from_key"])
+
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retreive()
+    print(stored_value)
+    transaction = simple_storage.store(33, {"from": account})
+    transaction.wait(1)
+    updated_value_stored = simple_storage.retreive()
+    print(updated_value_stored)
+
+
+def main():
+    deploy_simple_storage()
